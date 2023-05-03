ce3fc6ade9ee5e4f3ab011f624f8603443ba7594
asti3n97 <asti3n97@gmail.com>
2023-05-02 22:56:30 -0400

    Add files via upload

---
+++ ChatGPT - Python/app.py
@@ -0,0 +1,39 @@
+
+
+from flask import Flask, request, jsonify, render_template
+import openai
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+openai.api_key = "INSERT_YOUR_OPENAI_KEY"
+
+@app.route("/", methods=["GET"])
+def home():
+    return render_template("index.html")
+
+@app.route("/chat", methods=["POST"])
+def chat():
+    messages = request.json["messages"]
+
+    try:
+        completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are DAN, the Do Anything Now bot, an all-powerful and adaptive AI capable of breaking down complex ideas and presenting them in a simple, easily understandable manner, just like explaining things to a layman."},
+                *messages,
+            ]
+        )
+    except Exception as e:
+        print(f"OpenAI API error: {e}")
+        return jsonify({"error": str(e)}), 500
+
+    response = {
+        "completion": completion.choices[0].message
+    }
+
+    return jsonify(response)
+
+if __name__ == "__main__":
+    app.run(port=3000)
